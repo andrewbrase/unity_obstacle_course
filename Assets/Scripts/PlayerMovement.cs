71e84e15612ccdac911b9691fd0189c00d2708a6
andrewbrase <andrewbrase1@yahoo.com>
2025-06-10 07:25:50 -0500

    adding camera rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,8 +3,9 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 15f;
+    public float playerMoveSpeed = 15f;
     public float playerJumpForce = 10f;
+    float playerCameraRotationSpeed = 0.1f;
     private Rigidbody playerRigidBody;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,31 +20,45 @@ public class PlayerMovement : MonoBehaviour
     {
         processMovement();
         processPlayerJump();
+        processCameraRotation();
     }
 
     void processMovement()
     {
         Vector3 moveDirection = Vector3.zero;
+        Transform cameraTransform = Camera.main.transform;
+
         if (Keyboard.current.wKey.isPressed)
         {
-            moveDirection += Vector3.forward;
+            moveDirection += new Vector3(cameraTransform.forward.x, 0, cameraTransform.forward.z);
         }
         if (Keyboard.current.sKey.isPressed)
         {
-            moveDirection += Vector3.back;
+            moveDirection += new Vector3(-cameraTransform.forward.x, 0, -cameraTransform.forward.z);
         }
         if (Keyboard.current.aKey.isPressed)
         {
-            moveDirection += Vector3.left;
+            moveDirection += new Vector3(-cameraTransform.right.x, 0, -cameraTransform.right.z);
         }
         if (Keyboard.current.dKey.isPressed)
         {
-            moveDirection += Vector3.right;
+            moveDirection += new Vector3(cameraTransform.right.x, 0, cameraTransform.right.z);
         }
+
         if (moveDirection != Vector3.zero)
         {
             moveDirection.Normalize();
-            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            transform.position += moveDirection * playerMoveSpeed * Time.deltaTime;
+        }
+    }
+
+    void processCameraRotation()
+    {
+        if (Mouse.current.rightButton.isPressed)
+        {
+            Vector2 mouseDelta = Mouse.current.delta.ReadValue();
+            transform.Rotate(Vector3.up, mouseDelta.x * playerCameraRotationSpeed);
+            Camera.main.transform.Rotate(Vector3.left, mouseDelta.y * playerCameraRotationSpeed);
         }
     }
 
